a7a7ab18bf38a165e13f837134386be44161556b
k.ryo <ryosuke.10117@gmail.com>
2022-12-20 18:36:33 +0900

    rust-test> integrated test 2

--- rust-test/src/lib.rs
+++ rust-test/src/lib.rs
@@ -1,14 +1,36 @@
-mod ex2;
-/// Generally, the first line is a brief summary describing the function.
-///
-/// The next lines present detailed documentation.
-/// Code blocks start with triple backticks. The code has an implicit `fn main()` inside and `extern crate <cratename>`,  
-/// which means you can just start writing code.
-///
-/// ```
-/// let result = rust_test::add(2, 3);
-/// assert_eq!(result, 5);
-/// ```
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub struct Pizza {
+    pub topping: String,
+    pub inches: u8,
 }
+
+impl Pizza {
+    pub fn pepperoni(inches: u8) -> Self {
+        Pizza::bake("pepperoni", inches)
+    }
+
+    pub fn mozzarella(inches: u8) -> Self {
+        Pizza::bake("mozzarella", inches)
+    }
+
+    fn bake(topping: &str, inches: u8) -> Self {
+        Pizza {
+            topping: String::from(topping),
+            inches,
+        }
+    }
+}
+
+// mod ex2;
+// /// Generally, the first line is a brief summary describing the function.
+// ///
+// /// The next lines present detailed documentation.
+// /// Code blocks start with triple backticks. The code has an implicit `fn main()` inside and `extern crate <cratename>`,
+// /// which means you can just start writing code.
+// ///
+// /// ```
+// /// let result = rust_test::add(2, 3);
+// /// assert_eq!(result, 5);
+// /// ```
+// pub fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
