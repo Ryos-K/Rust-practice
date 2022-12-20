3ed4fe3011b36d00a265a2b64f2273debed6a600
k.ryo <ryosuke.10117@gmail.com>
2022-12-20 18:25:29 +0900

    rust-test> intergration test

---
+++ rust-test/src/lib.rs
@@ -0,0 +1,14 @@
+mod ex2;
+/// Generally, the first line is a brief summary describing the function.
+///
+/// The next lines present detailed documentation.
+/// Code blocks start with triple backticks. The code has an implicit `fn main()` inside and `extern crate <cratename>`,  
+/// which means you can just start writing code.
+///
+/// ```
+/// let result = rust_test::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -1,35 +1,31 @@
 mod ex1;
 
-fn main() {
-    for i in (1..10).step_by(2) {
-        print!("{}", i);
-    }
-}
+fn main() {}
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+// fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
 
-#[cfg(test)]
-mod add_function_tests {
-    use super::*;
+// #[cfg(test)]
+// mod add_function_tests {
+//     use super::*;
 
-    #[test]
-    fn add_work() {
-        assert_eq!(add(1, 2), 3);
-        assert_eq!(add(10, 12), 22);
-        assert_eq!(add(5, -2), 3);
-    }
+//     #[test]
+//     fn add_work() {
+//         assert_eq!(add(1, 2), 3);
+//         assert_eq!(add(10, 12), 22);
+//         assert_eq!(add(5, -2), 3);
+//     }
 
-    #[test]
-    #[should_panic]
-    fn add_fails() {
-        assert_eq!(add(2, 2), 7);
-    }
+//     #[test]
+//     #[should_panic]
+//     fn add_fails() {
+//         assert_eq!(add(2, 2), 7);
+//     }
 
-    #[test]
-    #[ignore = "not yet reviewed"]
-    fn add_negatives() {
-        assert_eq!(add(-2, -2), -4);
-    }
-}
+//     #[test]
+//     #[ignore = "not yet reviewed"]
+//     fn add_negatives() {
+//         assert_eq!(add(-2, -2), -4);
+//     }
+// }
